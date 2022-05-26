f04aded1239b002c732bc271016f31eeda1028b3
Alex Yates <alex.yates@dlmconsultants.com>
2022-05-26 12:57:54 +0100

    Fixing the PDF conversion so it works locally and reduces repetition.

---
+++ src/respawn-pdfs.ps1
@@ -0,0 +1,26 @@
+﻿Add-type -AssemblyName office -ErrorAction SilentlyContinue
+Add-Type -AssemblyName microsoft.office.interop.powerpoint -ErrorAction SilentlyContinue
+
+Set-Location "$PSScriptRoot"
+
+$sourceFilesDir = "."
+$outputFileDir = $PSScriptRoot -replace ("src", "bin")
+
+if(Test-Path $outputFileDir){
+    Remove-Item $outputFileDir -Force -Recurse
+}
+
+New-Item -ItemType Directory $outputFileDir
+
+$ppt = New-Object -com powerpoint.application
+$opt = [Microsoft.Office.Interop.PowerPoint.PpSaveAsFileType]::ppSaveAsPDF
+
+Get-ChildItem $sourceFilesDir -Filter '*.pptx' | ForEach-Object {
+     $srcFileName = $_.Name
+     $outFileName = $srcFileName -replace (".pptx", ".pdf")
+     $srcFile = $_.FullName
+     $outFile = Join-Path -Path $outputFileDir -ChildPath $outFileName
+     Write-Output "Converting $srcFile to PDF. Saving to $outFile"
+     $pres = $ppt.Presentations.Open($srcFile)
+     $pres.SaveAs($outFile, $opt)
+}
